c58b76ec418a53d748ebbd316e6aabba4f6b7193
Domengo <dominicsengo@gmail.com>
2024-06-28 19:11:48 +0300

    chore: Update card images in Constants.ts

--- bluewave/components/constants/Constants.ts
+++ bluewave/components/constants/Constants.ts
@@ -1,6 +1,17 @@
+import realtime from "../../assets/realtime.png";
+import usage from "../../assets/usagetracking.png";
+import alerts from "../../assets/alerts.png";
+import dataanalytics from "../../assets/data analytics.png";
+import usermanagement from "../../assets/usermanagement.png";
+import mobile from "../../assets/mobile.png";
+import iot from "../../assets/iot.png";
+
+
+import { StaticImageData } from "next/image";
+
 export interface CardData {
     id: number;
-    image: string;
+    image: StaticImageData;
     alt: string;
     title: string;
     description: string[];
@@ -9,49 +20,49 @@ export interface CardData {
 export const cardData: CardData[] = [
     {
       id: 1,
-      image: "../../assets/realtime.png",
+      image: realtime,
       alt: "Real-time Monitoring",
       title: "Real-time Monitoring",
       description: ["Instant updates", "Data Accuracy", "Remote Accessibility"]
     },
     {
       id: 2,
-      image: "../../assets/usage.png",
+      image: usage,
       alt: "Usage Tracking",
       title: "Usage Tracking",
       description: ["Historical Data", "Usage Patterns", "Resource Optimization"]
     },
     {
       id: 3,
-      image: "../../assets/alerts.png",
+      image: alerts,
       alt: "Alerts",
       title: "Alerts",
       description: ["Immediate notifications", "Customizable Thresholds", "Preventive measures"]
     },
     {
       id: 4,
-      image: "../../assets/data analytics.png",
+      image: dataanalytics,
       alt: "Data Analysis",
       title: "Data Analysis",
       description: ["Comprehensive reports", "Trend analysis", "Predictive analytics"]
     },
     {
       id: 5,
-      image: "../../assets/usermanagement.png",
+      image: usermanagement,
       alt: "User Management",
       title: "User Management",
       description: ["Role-based access", "Secure authentication", "User activity logs"]
     },
     {
       id: 6,
-      image: "../../assets/mobile.png",
+      image: mobile,
       alt: "Mobile App Integration",
       title: "Mobile App Integration",
       description: ["Real-time alerts", "Remote Monitoring", "User-friendly interface"]
     },
     {
       id: 7,
-      image: "../../assets/iot.png",
+      image: iot,
       alt: "IoT Device Compatibility",
       title: "IoT Device Compatibility",
       description: ["Easy integration", "Wide device support", "Enhanced data collection"]
